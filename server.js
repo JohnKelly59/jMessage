13e93aa66c331972768f18b1944c7bf0871eb760
John K <john.kellyfitness@gmail.com>
2021-12-20 10:41:23 -0500

    added path:

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const bodyParser = require("body-parser");
 var https = require("https");
 app.use(
@@ -8,6 +9,8 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+app.use(express.static(path.resolve(__dirname, "./jmessage")));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
