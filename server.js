f43f1059961f24e8c2062625f22ba7040b87b237
John K <john.kellyfitness@gmail.com>
2021-12-21 12:01:33 -0500

    made deployable

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.use(express.static(path.resolve(__dirname, "./jmessage")));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
